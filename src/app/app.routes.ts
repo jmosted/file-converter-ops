fbff8dd8ce6de4802b2dec134ee8783e9daa5ca5
jmosted <jose.roman@unas.edu.pe>
2026-06-16 22:43:03 -0500

    Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './shared/pages/home-page/home-page';
+import {DataConverter} from './file-converter/pages/data-converter/data-converter';
+
 
 export const routes: Routes = [
   {
     path:'home',
     component: HomePage
   },
-  // {
-  //   path:'file-converter',
-  //   //component:
-  // },
+  {
+   path:'file-converter',
+   component: DataConverter
+  },
   {
     path:'**',
     redirectTo:'home'
